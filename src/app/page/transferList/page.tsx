8fb84c73ce134e390bdd38164abf153f356462dd
dennislky <dennis92622@gmail.com>
2024-01-25 23:57:22 +0800

    feat: init setup by create-next-app

---
+++ src/app/page/transferList/page.tsx
@@ -0,0 +1,10 @@
+import DataTable from '@/components/DataTable';
+import React from 'react';
+
+const transferList = () => {
+    return (
+        <DataTable />
+    )
+}
+
+export default transferList;
